bf944e47a06c891da412aa27f2b0405b676023e1
SpiffyTheSpaceman <jtk002@ucsd.edu>
2019-10-25 09:42:09 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/*----- constants -----*/ 
+/*----- app's state (variables) -----*/ 
+let turn, winner, board;
+/*----- cached element references -----*/ 
+/*----- event listeners -----*/ 
+/*----- functions -----*/
